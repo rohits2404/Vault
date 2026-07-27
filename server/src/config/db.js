0d37d08db47f7a359832af5a4e9f1d9e0c573187
Rohit Sharma <rohits8731@gmail.com>
2026-07-28 00:05:50 +0530

    Add MongoDB connection utility

---
+++ server/src/config/db.js
@@ -0,0 +1,11 @@
+import { connect } from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const conn = await connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error(`MongoDB connection error: ${err.message}`);
+        process.exit(1);
+    }
+};
